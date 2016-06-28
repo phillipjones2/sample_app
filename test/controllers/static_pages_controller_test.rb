ad56a2766515005401d2c145fd447cff9399f685
Phillip Jones <phillipemersonjones@gmail.com>
2016-06-28 09:06:42 -0500

    title helper funciton added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", 'Home'
+    assert_select "title", 'Ruby on Rails'
   end
 
   test "should get help" do
